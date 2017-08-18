5bc22329118852047db25d6240aa95f15480d775
Ken Dreyer <kdreyer@redhat.com>
2017-08-18 14:10:33 -0600

    rebuild.sh: update for f26

--- rebuild.sh
+++ rebuild.sh
@@ -4,10 +4,10 @@ set -ex
 # Rebuild kmod packages for a new kernel.
 
 # Set whatever kernel you want here:
-KERNEL=4.11.9-200.fc25.x86_64
+KERNEL=4.11.11-300.fc26.x86_64
 
 # From mock-rpmfusion-free package:
-MOCKCFG=fedora-25-x86_64-rpmfusion_free
+MOCKCFG=fedora-26-x86_64-rpmfusion_free
 
 mkdir -p $KERNEL
 cd $KERNEL
@@ -31,5 +31,5 @@ pkgs+=(../wl-kmod/wl-kmod-6.30.223.271-13.fc27.src.rpm)
 for pkg in ${pkgs[@]}; do
   mock -r $MOCKCFG --no-clean rebuild \
     ../${pkg} --define "kernels $KERNEL"
-  cp /var/lib/mock/fedora-25-x86_64/result/*.x86_64.rpm .
+  cp /var/lib/mock/fedora-26-x86_64/result/*.x86_64.rpm .
 done
